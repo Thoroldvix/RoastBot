60cb87de33240490ac2ff3cf6a27ad18fee77571
Alexey <dignitionn@gmail.com>
2023-05-03 15:47:10 +0300

    reformat

--- main.py
+++ main.py
@@ -11,6 +11,23 @@ openai.api_key = os.getenv('OPENAI_KEY')
 discord_token = os.getenv('DISCORD_TOKEN')
 
 
+def get_roast_response(user_id):
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt='Напиши креативное оскорбление в стиле шекспира',
+            temperature=0.8,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.61
+        )
+        response_str = response['choices'][0]['text'].replace('"', '')
+        return f'{response_str} <:sirO:755463220264960080> <@{user_id}>'
+    except Exception as e:
+        sys.exit(e)
+
+
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game('🍷'))
@@ -27,20 +44,3 @@ async def on_reaction_add(reaction, user):
 
 
 client.run(discord_token)
-
-
-def get_roast_response(user_id):
-    try:
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt='Напиши креативное оскорбление в стиле шекспира',
-            temperature=0.8,
-            max_tokens=256,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0.61
-        )
-        response_str = response['choices'][0]['text'].replace('"', '')
-        return f'{response_str} <:sirO:755463220264960080> <@{user_id}>'
-    except Exception as e:
-        sys.exit(e)
